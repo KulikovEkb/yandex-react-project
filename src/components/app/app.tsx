bdd86de3d2c74174e7970d9df327bee38f5b452a
Konstantin Kulikov <konstantin.kulikov@sievo.com>
2022-12-12 19:40:09 +0200

    burger-ingredients.js done

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,22 +2,29 @@ import React from 'react';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import appStyles from './app.module.css';
+import BurgerConstructor from "../burger-constructor/burger-constructor";
 import data from "../../data/data.json";
 
 function App() {
+  const elements = {
+    top: data[0],
+    bottom: data[0],
+    fillers: [data[5], data[4], data[6], data[10], data[11], data[7], data[8], data[8]],
+    //fillers: [data[5], data[4]],
+  };
   const ingredients = {
     buns: [data[0], data[14]],
     sauces: [data[3], data[6], data[5], data[9]],
     //sauces: [data[3], data[6], data[5]],
     fillers: [data[13], data[11]],
-  }
+  };
 
   return (
     <>
       <AppHeader/>
       <div className={appStyles.App}>
         <BurgerIngredients ingredients={ingredients}/>
-        <BurgerIngredients ingredients={ingredients}/>
+        <BurgerConstructor elements={elements}/>
       </div>
     </>
   );
