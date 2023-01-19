7cb60a09a1d0549d7bb10f3d06f9064b801faf9f
Konstantin <kkmegarif@gmail.com>
2023-01-19 03:23:35 +0200

    ingredients smooth scrolling with refs

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,77 +1,78 @@
 import styles from './burger-ingredients.module.css';
 import scrollBarStyles from '../../utils/scroll-bar.module.css'
 import {Counter, CurrencyIcon, Tab} from "@ya.praktikum/react-developer-burger-ui-components";
-import React, {useState} from "react";
+import React, {forwardRef, useRef, useState} from "react";
 import PropTypes from "prop-types";
 import {ingredientShape} from "../../shapes/shapes";
 import IngredientDetails from "../ingredient-details/ingredient-details";
 import Modal from "../modal/modal";
 
 const BurgerIngredients = ({ingredients}) => {
+  const tabsRef = useRef({});
+
   if (!ingredients || Object.keys(ingredients).length === 0) return null;
 
   return (
     <div className={`${styles.ingredients} pt-10`}>
-      <Header/>
-      <IngredientsSection ingredientsData={ingredients}/>
+      <Header ref={tabsRef}/>
+      <IngredientsSection ref={tabsRef} ingredientsData={ingredients}/>
     </div>
   );
 }
 
-const Header = () => {
+const Header = forwardRef((props, ref) => {
   return (
     <div className={styles.header}>
       <p className='text text_type_main-large'>Соберите бургер</p>
-      <IngredientsTab/>
+      <IngredientsTab ref={ref}/>
     </div>
   );
-}
+});
 
-const IngredientsTab = () => {
-  const [current, setCurrent] = React.useState('buns')
+const IngredientsTab = forwardRef((props, ref) => {
+  const [current, setCurrent] = React.useState('Булки');
 
   const onTabClick = (tab) => {
     setCurrent(tab);
 
-    const tabElement = document.getElementById(tab);
-    if (tabElement) tabElement.scrollIntoView({behavior: 'smooth'});
+    ref.current[tab].scrollIntoView({behavior: 'smooth'});
   }
 
   return (
     <div className={`${styles.ingredientsTab}`}>
-      <Tab value='buns' active={current === 'buns'} onClick={onTabClick}>
+      <Tab value='Булки' active={current === 'Булки'} onClick={onTabClick}>
         Булки
       </Tab>
-      <Tab value='sauces' active={current === 'sauces'} onClick={onTabClick}>
+      <Tab value='Соусы' active={current === 'Соусы'} onClick={onTabClick}>
         Соусы
       </Tab>
-      <Tab value='fillers' active={current === 'fillers'} onClick={onTabClick}>
+      <Tab value='Начинки' active={current === 'Начинки'} onClick={onTabClick}>
         Начинки
       </Tab>
     </div>
   );
-}
+});
 
-const IngredientsSection = ({ingredientsData}) => {
+const IngredientsSection = forwardRef(({ingredientsData}, ref) => {
   return (
     <div className={`${styles.ingredientsSection} ${scrollBarStyles.scrollBar}`}>
-      <IngredientsCards id='buns' ingredients={ingredientsData.buns} header='Булки'/>
-      <IngredientsCards id='sauces' ingredients={ingredientsData.sauces} header='Соусы'/>
-      <IngredientsCards id='fillers' ingredients={ingredientsData.fillers} header='Начинки'/>
+      <IngredientsCards ref={ref} ingredients={ingredientsData.buns} header='Булки'/>
+      <IngredientsCards ref={ref} ingredients={ingredientsData.sauces} header='Соусы'/>
+      <IngredientsCards ref={ref} ingredients={ingredientsData.fillers} header='Начинки'/>
     </div>
   );
-}
+});
 
-const IngredientsCards = ({id, ingredients, header}) => {
+const IngredientsCards = forwardRef(({ingredients, header}, ref) => {
   return (
-    <div className={styles.cards} id={id}>
+    <div className={styles.cards} ref={x => ref.current[header] = x}>
       <p className='text text_type_main-medium'>{header}</p>
       <div className={`${styles.cardsList} pl-4 pr-2`}>
         {ingredients.map(ingredient => <IngredientCard key={ingredient._id} data={ingredient}/>)}
       </div>
     </div>
   );
-}
+});
 
 const IngredientCard = ({data}) => {
   const [isOpen, setIsOpen] = useState(false);
