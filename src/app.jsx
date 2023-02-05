778d1be8651b75c5189e048e509f733d39303af0
Konstantin <kkmegarif@gmail.com>
2023-02-05 17:21:24 +0200

    Login page

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import ErrorBoundary from "./helpers/error-boundary";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Constructor from "./pages/constructor";
 import NotFound from "./pages/not-found";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -12,6 +13,18 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Constructor/>}/>
+          <Route path="/login" element={<Login/>}/>
+          {/*todo(kulikov): implement
+          <Route path="/register" element={<Register/>}/>
+          todo(kulikov): implement
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+          todo(kulikov): implement
+          <Route path="/reset-password" element={<ResetPassword/>}/>
+          todo(kulikov): implement
+          <Route path="/profile" element={<Profile/>}/>
+          todo(kulikov): implement
+          <Route path="/ingredients/:id" element={<Ingredient/>}/>
+          */}
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/login.js
@@ -0,0 +1,43 @@
+import {Button, EmailInput, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components";
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = React.useState('')
+  const onEmailChange = e => {
+    setEmail(e.target.value)
+  }
+  const [passwordValue, setPasswordValue] = React.useState('')
+  const onPasswordChange = e => {
+    setPasswordValue(e.target.value)
+  }
+
+  // todo(kulikov): refactor
+  return (
+    <div style={{position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <p className='text text_type_main-medium'>Вход</p>
+
+      <div className='mt-6' style={{display: 'flex', flexDirection: 'column', gap: '24px'}}>
+        <EmailInput value={email} onChange={onEmailChange}/>
+        <PasswordInput value={passwordValue} onChange={onPasswordChange}/>
+      </div>
+
+      <div className='mt-6 mb-20'>
+        <Button htmlType="button" type="primary" size="large">
+          Войти
+        </Button>
+      </div>
+
+      <div style={{display: 'flex', flexDirection: 'column', gap: '16px', alignItems: 'center'}}>
+        <p className='text text_type_main-default text_color_inactive'>
+          Вы — новый пользователь? <Link to='/register' className='text_color_accent'>Зарегистрироваться</Link>
+        </p>
+        <p className='text text_type_main-default text_color_inactive'>
+          Забыли пароль? <Link to='/forgot-password' className='text_color_accent'>Восстановить пароль</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
