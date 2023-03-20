ec4728dc339733a5c86a5e18cea21e4d91cb4d84
Konstantin Kulikov <kkmegarif@gmail.com>
2023-03-21 00:48:37 +0200

    redux TS init

--- src/components/burger-ingredients/store/ingredients-actions.ts
+++ src/components/burger-ingredients/store/ingredients-actions.ts
@@ -1,13 +1,25 @@
 import * as normaClient from "../../../clients/norma-client";
 import {ADD_BUN, ADD_INGREDIENT} from "../../burger-constructor/store/constructor-actions";
+import {TIngredient} from "../../../types/ingredient";
 
-export const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST';
-export const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS';
-export const GET_INGREDIENTS_FAIL = 'GET_INGREDIENTS_FAIL';
+export const GET_INGREDIENTS_REQUEST: 'GET_INGREDIENTS_REQUEST' = 'GET_INGREDIENTS_REQUEST';
+export const GET_INGREDIENTS_SUCCESS: 'GET_INGREDIENTS_SUCCESS' = 'GET_INGREDIENTS_SUCCESS';
+export const GET_INGREDIENTS_FAIL: 'GET_INGREDIENTS_FAIL' = 'GET_INGREDIENTS_FAIL';
 
-export const SET_BUN_ID = 'SET_BUN_COUNTER';
-export const INCREMENT_INGREDIENT_COUNTER = 'INCREMENT_INGREDIENT_COUNTER';
-export const DECREMENT_INGREDIENT_COUNTER = 'DECREMENT_INGREDIENT_COUNTER';
+export const SET_BUN_ID: 'SET_BUN_COUNTER' = 'SET_BUN_COUNTER';
+export const INCREMENT_INGREDIENT_COUNTER: 'INCREMENT_INGREDIENT_COUNTER' = 'INCREMENT_INGREDIENT_COUNTER';
+export const DECREMENT_INGREDIENT_COUNTER: 'DECREMENT_INGREDIENT_COUNTER' = 'DECREMENT_INGREDIENT_COUNTER';
+
+export interface IGetIngredientsAction {
+  readonly type: 'GET_INGREDIENTS_REQUEST';
+}
+export interface IGetIngredientsSuccess {
+  readonly type: 'GET_INGREDIENTS_SUCCESS';
+  readonly ingredients: TIngredient[];
+}
+export interface IGetIngredientsFailedAction {
+  readonly type: 'GET_INGREDIENTS_FAIL';
+}
 
 export function getIngredients() {
   return async function (dispatch) {
