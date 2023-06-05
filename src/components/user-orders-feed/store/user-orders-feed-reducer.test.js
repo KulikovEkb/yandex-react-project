dbc893001398908ad44aa35d5e21ce89b653e0e7
Konstantin Kulikov <kkmegarif@gmail.com>
2023-06-05 03:11:08 +0300

    user-orders-feed-reducer.test.js

--- src/components/user-orders-feed/store/user-orders-feed-reducer.test.js
+++ src/components/user-orders-feed/store/user-orders-feed-reducer.test.js
@@ -1,40 +1,38 @@
-import {ordersFeedReducer} from "./orders-feed-reducer";
+import {userOrdersFeedReducer} from "./user-orders-feed-reducer";
 import {
-  ORDERS_FEED_CONNECTION_CLOSED,
-  ORDERS_FEED_CONNECTION_ERROR,
-  ORDERS_FEED_CONNECTION_SUCCESS, ORDERS_FEED_GET_MESSAGE
-} from "./orders-feed-actions";
+  USER_ORDERS_FEED_CONNECTION_CLOSED,
+  USER_ORDERS_FEED_CONNECTION_ERROR,
+  USER_ORDERS_FEED_CONNECTION_SUCCESS, USER_ORDERS_FEED_GET_MESSAGE
+} from "./user-orders-feed-actions";
 
 const initialState = {
   isConnected: false,
-  total: 0,
-  totalToday: 0,
   orders: [],
 };
 
-describe('orders feed reducer', function () {
+describe('user orders feed reducer', function () {
   it('should return the initial state', function () {
-    expect(ordersFeedReducer(undefined, {})).toEqual(initialState);
+    expect(userOrdersFeedReducer(undefined, {})).toEqual(initialState);
   });
 
   it('should set connection opened', function () {
-    expect(ordersFeedReducer({...initialState}, {type: ORDERS_FEED_CONNECTION_SUCCESS}))
+    expect(userOrdersFeedReducer({...initialState}, {type: USER_ORDERS_FEED_CONNECTION_SUCCESS}))
       .toEqual({...initialState, isConnected: true, error: undefined})
   });
 
   it('should set connection failed', function () {
     const errorEvent = new ErrorEvent('test error type');
 
-    expect(ordersFeedReducer({...initialState}, {type: ORDERS_FEED_CONNECTION_ERROR, error: errorEvent}))
+    expect(userOrdersFeedReducer({...initialState}, {type: USER_ORDERS_FEED_CONNECTION_ERROR, error: errorEvent}))
       .toEqual({...initialState, error: errorEvent})
   });
 
   it('should set connection closed', function () {
-    expect(ordersFeedReducer({...initialState, isConnected: true}, {type: ORDERS_FEED_CONNECTION_CLOSED}))
+    expect(userOrdersFeedReducer({...initialState, isConnected: true}, {type: USER_ORDERS_FEED_CONNECTION_CLOSED}))
       .toEqual({...initialState, isConnected: false})
   });
 
-  it('should set orders', function () {
+  it('should set user orders', function () {
     const message = {
       success: true,
       orders: [
@@ -52,11 +50,9 @@ describe('orders feed reducer', function () {
           number: 46149,
         }
       ],
-      "total": 46058,
-      "totalToday": 231
     };
 
-    expect(ordersFeedReducer({...initialState}, {type: ORDERS_FEED_GET_MESSAGE, message: JSON.stringify(message)}))
-      .toEqual({...initialState, orders: message.orders, total: message.total, totalToday: message.totalToday})
+    expect(userOrdersFeedReducer({...initialState}, {type: USER_ORDERS_FEED_GET_MESSAGE, message: JSON.stringify(message)}))
+      .toEqual({...initialState, orders: message.orders})
   });
 });
